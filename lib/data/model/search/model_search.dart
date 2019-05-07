0519f7e1d94f64e3636c0c871c7cf16acebc4bbc
JK TAN <jktan@JKs-MacBook-Pro.local>
2019-05-07 11:44:55 +0800

    done youtube network data source API

---
+++ lib/data/model/search/model_search.dart
@@ -0,0 +1,9 @@
+export 'id.dart';
+export 'search_item.dart';
+export 'search_snippet.dart';
+export 'youtube_search_result.dart';
+export 'youtube_search_error.dart';
+
+// temp
+export 'thumbnails.dart';
+export 'thumbnail.dart';
